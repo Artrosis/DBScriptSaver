7446345c16c7c949bf8e8f528c59eadc7a39fb36
Артур <a.petrov@rivc-pulkovo.ru>
2020-09-21 15:54:23 +0300

    форматирование

--- ScriptWrapper.cs
+++ ScriptWrapper.cs
@@ -27,19 +27,16 @@ namespace DBScriptSaver
                 return result;
             }
         }
-
         public ScriptWrapper(Script t)
         {
             this.t = t;
         }
-
         public string FileName => getScript.FileName;
         public string FullPath => getScript.FullPath;
         public string ScriptText => getScript.ScriptText;
         public string ObjectType => getScript.ObjectType;
         public string ChangeState => getScript.ChangeState;
         public bool Save { get; set; } = false;
-
         public string EditedFilePath { get; internal set; }
     }
 }
