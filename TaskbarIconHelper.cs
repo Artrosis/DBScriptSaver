349faf470f99b76786216a53dd1aaa2f90a09426
Артур <a.petrov@rivc-pulkovo.ru>
2021-02-18 10:32:31 +0300

    подсказки

--- TaskbarIconHelper.cs
+++ TaskbarIconHelper.cs
@@ -16,7 +16,7 @@ namespace DBScriptSaver
 {
     public class TaskbarIconHelper
     {
-        static TaskbarIcon tbi = new TaskbarIcon();
+        static readonly TaskbarIcon tbi = new TaskbarIcon();
         internal static void CreateTaskbarIcon()
         {
             Stream iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/DBScriptSaver;component/ico/main.ico")).Stream;
@@ -33,9 +33,11 @@ namespace DBScriptSaver
 
             foreach (var proj in Settings.Projects)
             {
-                var ProjectItem = new MenuItem();
-                ProjectItem.Header = proj.Name;
-                ProjectItem.Tag = proj;
+                var ProjectItem = new MenuItem
+                {
+                    Header = proj.Name,
+                    Tag = proj
+                };
                 menu.Items.Add(ProjectItem);
 
                 foreach (var db in proj.DataBases)
@@ -50,23 +52,28 @@ namespace DBScriptSaver
                     }
                     catch (Exception){}
 
-                    var DBItem = new MenuItem();
-
-                    DBItem.IsEnabled = false;
-                    DBItem.Header = db.Name + (!HasConnect? @"(Нет подключения)":"");
-                    DBItem.Tag = db;
+                    var DBItem = new MenuItem
+                    {
+                        IsEnabled = false,
+                        Header = db.Name + (!HasConnect ? @"(Нет подключения)" : ""),
+                        Tag = db
+                    };
                     ProjectItem.Items.Add(DBItem);
 
-                    var AddScriptItem = new MenuItem();
-                    AddScriptItem.Header = @"Добавить миграцию";
-                    AddScriptItem.Tag = db;
+                    var AddScriptItem = new MenuItem
+                    {
+                        Header = @"Добавить миграцию",
+                        Tag = db
+                    };
                     AddScriptItem.PreviewMouseDown += AddScriptItem_PreviewMouseDown; ;
                     AddScriptItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/add_script.png")), Width = 16, Height = 16 };
                     ProjectItem.Items.Add(AddScriptItem);
 
-                    var DBDependenciesItem = new MenuItem();
-                    DBDependenciesItem.Header = @"Зависимости развёртывания";
-                    DBDependenciesItem.Tag = db;
+                    var DBDependenciesItem = new MenuItem
+                    {
+                        Header = @"Зависимости развёртывания",
+                        Tag = db
+                    };
                     DBDependenciesItem.PreviewMouseDown += DBDependenciesItem_PreviewMouseDown;
                     DBDependenciesItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/chain.png")), Width = 16, Height = 16 };
                     ProjectItem.Items.Add(DBDependenciesItem);
@@ -76,16 +83,20 @@ namespace DBScriptSaver
                         continue;
                     }
 
-                    var UpdateDBItem = new MenuItem();
-                    UpdateDBItem.Header = @"Обновить";
-                    UpdateDBItem.Tag = db;
+                    var UpdateDBItem = new MenuItem
+                    {
+                        Header = @"Обновить",
+                        Tag = db
+                    };
                     UpdateDBItem.PreviewMouseDown += UpdateDBItem_PreviewMouseDown;
                     UpdateDBItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/Refresh.png")), Width = 16, Height = 16 };
                     ProjectItem.Items.Add(UpdateDBItem);
 
-                    var DBSettingsItem = new MenuItem();
-                    DBSettingsItem.Header = @"Настройки объектов БД";
-                    DBSettingsItem.Tag = db;
+                    var DBSettingsItem = new MenuItem
+                    {
+                        Header = @"Настройки объектов БД",
+                        Tag = db
+                    };
                     DBSettingsItem.PreviewMouseDown += DBSettingsItem_PreviewMouseDown; ;
                     DBSettingsItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/Settings.png")), Width = 16, Height = 16 };
                     ProjectItem.Items.Add(DBSettingsItem);
@@ -93,9 +104,11 @@ namespace DBScriptSaver
                     ProjectItem.Items.Add(new Separator());
                 }
 
-                var ProjectSettingsItem = new MenuItem();
-                ProjectSettingsItem.Header = @"Настройки проекта";
-                ProjectSettingsItem.Tag = proj;
+                var ProjectSettingsItem = new MenuItem
+                {
+                    Header = @"Настройки проекта",
+                    Tag = proj
+                };
                 ProjectSettingsItem.PreviewMouseDown += ProjectSettingsItem_PreviewMouseDown; ;
                 ProjectSettingsItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/Settings.png")), Width = 16, Height = 16 };
                 ProjectItem.Items.Add(ProjectSettingsItem);
@@ -103,16 +116,20 @@ namespace DBScriptSaver
 
             menu.Items.Add(new Separator());
 
-            var SettingsItem = new MenuItem();
-            SettingsItem.Header = "Настройки";
+            var SettingsItem = new MenuItem
+            {
+                Header = "Настройки"
+            };
             SettingsItem.PreviewMouseDown += SettingsItem_PreviewMouseDown;
             SettingsItem.Icon = new Image() { Source = new BitmapImage(new Uri("pack://application:,,,/DBScriptSaver;component/img/Settings.png")), Width = 16, Height = 16 };
             menu.Items.Add(SettingsItem);
 
             menu.Items.Add(new Separator());
 
-            var CloseItem = new MenuItem();
-            CloseItem.Header = "Выход";
+            var CloseItem = new MenuItem
+            {
+                Header = "Выход"
+            };
             CloseItem.PreviewMouseDown += CloseItem_PreviewMouseDown;
             menu.Items.Add(CloseItem);
 
@@ -121,9 +138,7 @@ namespace DBScriptSaver
 
         private static void DBDependenciesItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            var DB = ((MenuItem)sender).Tag as ProjectDataBase;
-
-            if (DB == null)
+            if (!(((MenuItem)sender).Tag is ProjectDataBase DB))
             {
                 return;
             }
@@ -133,9 +148,7 @@ namespace DBScriptSaver
 
         private static void AddScriptItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            var DB = ((MenuItem)sender).Tag as ProjectDataBase;
-
-            if (DB == null)
+            if (!(((MenuItem)sender).Tag is ProjectDataBase DB))
             {
                 return;
             }
@@ -145,9 +158,7 @@ namespace DBScriptSaver
 
         private static void DBSettingsItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            var DB = ((MenuItem)sender).Tag as ProjectDataBase;
-
-            if (DB == null)
+            if (!(((MenuItem)sender).Tag is ProjectDataBase DB))
             {
                 return;
             }
@@ -164,9 +175,7 @@ namespace DBScriptSaver
 
         private static void UpdateDBItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            var DB = ((MenuItem)sender).Tag as ProjectDataBase;
-
-            if (DB == null)
+            if (!(((MenuItem)sender).Tag is ProjectDataBase DB))
             {
                 return;
             }
@@ -184,9 +193,7 @@ namespace DBScriptSaver
 
         private static void ProjectSettingsItem_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            var proj = ((MenuItem)sender).Tag as Project;
-
-            if (proj == null)
+            if (!(((MenuItem)sender).Tag is Project proj))
             {
                 return;
             }
